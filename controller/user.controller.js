d0932523cc79dd35dea93768b5e0c96aeb75608a
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-09-24 08:59:09 +0530

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,23 +1,23 @@
 const User = require('../model/user.model')
 const getAllUsers = async (req, res) => {
     const users = await User.find();
-    res.status(200).json(users);
+    res.send(users);
 }
 
 const getUserById = async (req, res) => {
     const userId = req.params.id;
     const user = await User.findById(userId);
     if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.send({ message: 'User not found' });
     }
-    res.status(200).json(user);
+    res.send(user);
 }
 
 const createUser = async (req, res) => {
     const { username, email, password } = req.body;
     const user = new User({ username, email, password });
     const newUser = await user.save();
-    res.status(201).json(newUser);
+    res.send(newUser);
 
 }
 
@@ -25,28 +25,28 @@ const updateUser = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.send({ message: 'User not found' });
     }
-    res.status(200).json(updatedUser);
+    res.send(updatedUser);
 }
 
 const deleteUser = async (req, res) => {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
 
     if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.send({ message: 'User not found' });
     }
-    res.status(200).json({ message: 'User deleted successfully' });
+    res.send({ message: 'User deleted successfully' });
 }
 
 const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.send({ message: 'User not found' });
     }
     if (user.password !== password) {
-        return res.status(401).json({ message: 'Invalid password' });
+        return res.send({ message: 'Invalid password' });
     }
     res.cookie('id',user.id).redirect('/')
 }
@@ -60,7 +60,6 @@ const signup = async (req, res) => {
     console.log(req.file);
     const user = new User({ username, email, password, profile });
     const newUser = await user.save();
-    // res.status(201).json(newUser);
     res.redirect('/user/login');
 }
 const signuppage = (req, res) => {
